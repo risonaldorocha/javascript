bfb028b2d0914642b30f9427ead9c045cc0b5a28
Risonaldo Rocha <105023132+risonaldorocha@users.noreply.github.com>
2022-07-17 18:50:05 -0300

    dia 17

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais ="BRASIL"
+console.log(`Vivendo em ${pais}`)
+if (pais == "BRASIL") {
+    console.log('você é brasileiro')
+} else {
+    console.log('você é Estrangeiro')
+}
