ecc21b9329cb9bd70fc0c1088ca7d8630488889f
Risonaldo Rocha <105023132+risonaldorocha@users.noreply.github.com>
2022-07-27 22:56:35 -0300

    27

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -1,1 +1,22 @@
-alert (`Olá`)
+function carregar() {
+    var msg = window.document.getElementById(`msg`)
+    var img = window.document.getElementById(`imagem`)
+    var data = new Date()
+   // var hora = data.getHours()
+   var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background =`#fed941`
+    } else if (hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = `#7d7c75`
+    } else{
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = `#232e4c`
+    } 
+}
+    
