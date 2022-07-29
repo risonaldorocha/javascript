242aed125ab81f945f95e403e1d1197954b9cb38
Risonaldo Rocha <105023132+risonaldorocha@users.noreply.github.com>
2022-07-28 23:04:26 -0300

    28 JUL

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -1,1 +1,11 @@
-alert (`Olá`)
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert(`ERRO`)
+    } else {
+        Window.alert('TUDO OK')
+    }
+}
