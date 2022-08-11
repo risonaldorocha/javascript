5ad64c40d6b38178ab3bfa66f4eb5013426b3ed4
Risonaldo Rocha <105023132+risonaldorocha@users.noreply.github.com>
2022-08-11 16:57:38 -0300

    11 ago

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -1,10 +1,17 @@
 function verificar(){
    var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var f = document.getElementById('txtano')
+    var fano = Number(f.value)
     var res = document.querySelector('div#res')
-   // document.write(` data:${data} ANO ${ano} fano ${fano} res ${res}`)
+  // document.write(` data:${data} ANO ${ano} fano ${fano} res ${res}`)
   if (fano == 0 || fano >= ano){
-      d
+      document.write(`[ERRO] - digite valor correto`)
+  } else if (fano < 12){
+    document.write(`CRIANÇA`)
+  } else if (fano < 50) {
+    document.write(`JOVEM`)
+  } else {
+    document.write(`IDOSO`)
   }
 }   
