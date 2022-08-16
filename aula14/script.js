151f7f60a2ac60fb22ebbe95bb382042f44f4113
Risonaldo Rocha <105023132+risonaldorocha@users.noreply.github.com>
2022-08-16 18:00:18 -0300

    16ago

--- aula14/script.js
+++ aula14/script.js
@@ -7,7 +7,7 @@ var txtf = window.document.querySelector(`input#txtfim`)
 var fim = Number(txtf.value)
 var txtp = window.document.querySelector(`input#txtpasso`)
 var passo= Number(txtpasso.value)
-var res = wind.document.querySelector(`div#res`)
+var res = window.document.querySelector(`div#res`)
 while (ini < fim) {
     res.innerHTML = `${ini}`
     ini = ini + passo
